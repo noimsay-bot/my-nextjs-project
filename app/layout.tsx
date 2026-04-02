13455a6512c7ece7e54680b5b667433e74345ffc
noimsay <noimsay@example.com>
2026-04-02 23:32:32 +0900

    홈 4.2 수정v5

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  minimumScale: 0.5,
-  maximumScale: 5,
+  minimumScale: 0.65,
+  maximumScale: 3,
   userScalable: true,
 };
 
